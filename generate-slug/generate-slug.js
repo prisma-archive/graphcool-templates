97e5f0263adff152c2aef171d47a50d01ed366c1
Nilan Marktanner <nilan.marktanner@gmail.com>
2017-05-25 23:26:39 +0200

    adjust generate slug function

--- generate-slug/generate-slug.js
+++ generate-slug/generate-slug.js
@@ -1,5 +1,5 @@
 'use strict';
-module.exports = function (event, cb) {
+module.exports = function (event) {
   let name = event.data.name
   const slug = name.toLowerCase().replace(' ', '-')
   const response = Object.assign({}, event.data, {slug})
