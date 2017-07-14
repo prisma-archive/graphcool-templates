9760cd5b34d107dd83ec055816bf131c06e9549e
Mike Wickett <mwickett@users.noreply.github.com>
2017-07-14 09:58:56 -0400

    Update slug function to handle more cases (#20)
    * Update slug function to handle more cases
    
    * Updated with comments for regex and added additional filters
    
    * Added myself to the readme
    
    * indentation

--- generate-slug/generate-slug.js
+++ generate-slug/generate-slug.js
@@ -1,7 +1,15 @@
 'use strict';
 module.exports = function (event) {
-  let name = event.data.name
-  const slug = name.toLowerCase().replace(' ', '-')
+  let title = event.data.title
+  const slug = title
+    .toString()
+    .toLowerCase()
+    .trim()
+    .replace(/\s+/g, "-")     // Replace all spaces with -
+    .replace(/&/g, '-and-')   // Replace & with 'and'
+    .replace(/[^\w\-]+/g, "") // Remove all non-word characters
+    .replace(/\-\-+/g, "-")   // Replace multiple - with single -
+
   const response = Object.assign({}, event.data, {slug})
 
   return {
