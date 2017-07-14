37973d46b729f6fadc7e5272325ae1c5d9e4a32b
Mike Wickett <mike@sagecomm.com>
2017-07-14 09:22:48 -0400

    Update slug function to handle more cases

--- generate-slug/generate-slug.js
+++ generate-slug/generate-slug.js
@@ -1,7 +1,14 @@
 'use strict';
 module.exports = function (event) {
-  let name = event.data.name
-  const slug = name.toLowerCase().replace(' ', '-')
+  let title = event.data.title
+  const slug = title
+  	.toLowerCase()
+  	.trim()
+  	.replace(/\s+/g, "-")
+  	.replace(/[^\w\-]+/g, "")
+    .replace(/\-\-+/g, "-")
+    .replace(/^-+/, "")
+    .replace(/-+$/, "")
   const response = Object.assign({}, event.data, {slug})
 
   return {
