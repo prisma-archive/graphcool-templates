43894bb76fb98c0e58cfd4c1dcc96835a96250c8
Mike Wickett <mike@sagecomm.com>
2017-07-14 09:32:56 -0400

    Updated with comments for regex and added additional filters

--- generate-slug/generate-slug.js
+++ generate-slug/generate-slug.js
@@ -2,13 +2,14 @@
 module.exports = function (event) {
   let title = event.data.title
   const slug = title
+    .toString()
   	.toLowerCase()
   	.trim()
-  	.replace(/\s+/g, "-")
-  	.replace(/[^\w\-]+/g, "")
-    .replace(/\-\-+/g, "-")
-    .replace(/^-+/, "")
-    .replace(/-+$/, "")
+    .replace(/\s+/g, "-")     // Replace all spaces with -
+    .replace(/&/g, '-and-')   // Replace & with 'and'
+  	.replace(/[^\w\-]+/g, "") // Remove all non-word characters
+    .replace(/\-\-+/g, "-")   // Replace multiple - with single -
+
   const response = Object.assign({}, event.data, {slug})
 
   return {
