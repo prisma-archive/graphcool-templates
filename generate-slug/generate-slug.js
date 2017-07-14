5810da22e5ad8344480fbb4ea5f47a459494f007
Nilan Marktanner <nilan.marktanner@student.kit.edu>
2017-07-14 15:58:23 +0200

    indentation

--- generate-slug/generate-slug.js
+++ generate-slug/generate-slug.js
@@ -3,11 +3,11 @@ module.exports = function (event) {
   let title = event.data.title
   const slug = title
     .toString()
-  	.toLowerCase()
-  	.trim()
+    .toLowerCase()
+    .trim()
     .replace(/\s+/g, "-")     // Replace all spaces with -
     .replace(/&/g, '-and-')   // Replace & with 'and'
-  	.replace(/[^\w\-]+/g, "") // Remove all non-word characters
+    .replace(/[^\w\-]+/g, "") // Remove all non-word characters
     .replace(/\-\-+/g, "-")   // Replace multiple - with single -
 
   const response = Object.assign({}, event.data, {slug})
