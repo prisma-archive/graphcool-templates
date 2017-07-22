ed34509f42a1238c8b152fe6759187e1e13032f7
Nilan Marktanner <nilan.marktanner@student.kit.edu>
2017-07-22 16:58:34 +0200

    quotes

--- sendgrid-email/addtolist_es6.js
+++ sendgrid-email/addtolist_es6.js
@@ -1,13 +1,13 @@
 'use latest'
 module.exports = (event) => {
-  let sg = require("sendgrid")(___SENDGRID_API_KEY___)
+  let sg = require('sendgrid')(___SENDGRID_API_KEY___)
 
   return new Promise((resolve, reject) => {
     let request = sg.emptyRequest()
     const { email, nameFirst: first_name, nameLast: last_name } = event.data.User.node
     request.body = [{ email, first_name, last_name }]
-    request.method = "POST"
-    request.path = "/v3/contactdb/recipients"
+    request.method = 'POST'
+    request.path = '/v3/contactdb/recipients'
 
     callAPI(request).then(response => {
       let request = sg.emptyRequest()
