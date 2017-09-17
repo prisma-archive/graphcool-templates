2d89747c057b3a9be553866dbd3e9eb704ca1e1a
Martin Adams <heymartinadams@users.noreply.github.com>
2017-09-17 09:50:36 -0700

    Added Intercom.

--- miscellaneous/intercom/inline.js
+++ miscellaneous/intercom/inline.js
@@ -2,53 +2,54 @@ const fromEvent = require('graphcool-lib').fromEvent
 const crypto = require('crypto')
 
 module.exports = function (event) {
-	const email = event.data.email
-	const platform = event.data.platform
+  const email = event.data.email
+  const platform = event.data.platform
 
-	const graphcool = fromEvent(event)
-	const api = graphcool.api('simple/v1')
+  const graphcool = fromEvent(event)
+  const api = graphcool.api('simple/v1')
 
-	var apikey = ''
+  var apikey = ''
 
-	switch (platform) {
-		case 'web': apikey = '__WEB_API_KEY__'; break;
-		case 'ios': apikey = '__IOS_API_KEY__'; break;
-		case 'android': apikey = '__ANDROID_API_KEY__'; break;
-		default: apikey = '__WEB_API_KEY__'
-	}
+  switch (platform) {
+    // Add your API keys here
+    case 'web': apikey = __WEB_API_KEY__; break;
+    case 'ios': apikey = __IOS_API_KEY__; break;
+    case 'android': apikey = __ANDROID_API_KEY__; break;
+    default: apikey = __WEB_API_KEY__
+  }
 
-	function getGraphcoolUser(email) {
-		return api.request(`
-			query {
-				User(email: "${email}") {
-					id
-				}
-			}`)
-			.then(userQueryResult => {
-				if (userQueryResult.error) {
-					return Promise.reject(userQueryResult.error)
-				} else if (!userQueryResult.User || !userQueryResult.User.id) {
-					return Promise.reject('Invalid credentials')
-				}
-				return userQueryResult.User
-			})
-	}
+  function getGraphcoolUser(email) {
+    return api.request(`
+      query {
+        User(email: "${email}") {
+          id
+        }
+      }`)
+      .then(userQueryResult => {
+        if (userQueryResult.error) {
+          return Promise.reject(userQueryResult.error)
+        } else if (!userQueryResult.User || !userQueryResult.User.id) {
+          return Promise.reject('Invalid credentials')
+        }
+        return userQueryResult.User
+      })
+  }
 
-	function generateHash(userId) {
-		const hmac = crypto.createHmac('sha256', apikey)
-		hmac.update(userId)
-		return hmac.digest('hex')
-	}
+  function generateHash(userId) {
+    const hmac = crypto.createHmac('sha256', apikey)
+    hmac.update(userId)
+    return hmac.digest('hex')
+  }
 
-	return getGraphcoolUser(email)
-		.then(graphcoolUser => {
-			return generateHash(graphcoolUser.id)
-		})
-		.then(hash => {
-			return { data: { hash } }
-		})
-		.catch(error => {
-			console.log(error)
-			return { error: 'An unexpected error occured. Please contact customer support.' }
-		})
+  return getGraphcoolUser(email)
+    .then(graphcoolUser => {
+      return generateHash(graphcoolUser.id)
+    })
+    .then(hash => {
+      return { data: { hash } }
+    })
+    .catch(error => {
+      console.log(error)
+      return { error: 'An unexpected error occured. Please contact customer support.' }
+    })
 }
