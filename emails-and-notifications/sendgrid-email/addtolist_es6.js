d58d6a7cc9461246a8f13fc9951e80335268e3cd
Hugo de Côté <hvillain@student.42.fr>
2017-10-11 11:53:24 +0200

    Update addtolist_es6.js
    Correction of "callAPI" missing argument + explanation of the process

--- emails-and-notifications/sendgrid-email/addtolist_es6.js
+++ emails-and-notifications/sendgrid-email/addtolist_es6.js
@@ -4,17 +4,23 @@ module.exports = (event) => {
 
   return new Promise((resolve, reject) => {
     let request = sg.emptyRequest()
+    
+    // add SG custom fields here
     const { email, nameFirst: first_name, nameLast: last_name } = event.data.User.node
     request.body = [{ email, first_name, last_name }]
     request.method = "POST"
     request.path = "/v3/contactdb/recipients"
 
-    callAPI(request).then(response => {
+    // this call creates a new recipient in Sendgrid
+    // and adds it to the global list "All contacts"
+    callAPI(sg, request).then(response => {
+      
+      // this call adds newly created recipient to a specific list (not required)
       let request = sg.emptyRequest()
       request.method = 'POST'
       request.path = '/v3/contactdb/lists/___SENDGRID_LIST_ID___/recipients/' + response.body.persisted_recipients[0]
 
-      callAPI(request)
+      callAPI(sg, request)
         .then(response => resolve(response))
         .catch(error => reject(error))
     })
