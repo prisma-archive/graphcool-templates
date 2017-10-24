15dde61dad72767f7d9838492684d062a4cfe2a6
Florian Didron <florian@111studio.jp>
2017-10-24 09:22:13 +0900

    Adds test App

---
+++ auth/auth0/test/index.js
@@ -0,0 +1,33 @@
+function displayAuthResults(accessToken, idToken) {
+  var el = document.getElementById('mutation');
+  el.style.visibility = 'visible';
+  var mutation = `
+  # Run this mutation in the Graphcool Playground to authenticate a user
+
+  mutation {
+    authenticateUser(
+      accessToken: "${accessToken}"
+      idToken: "${idToken}"
+    ) {
+      token
+    }
+  }
+  `;
+  el.innerHTML = mutation;
+}
+
+//Replace __CLIENT_ID__ and __AUTH0_DOMAIN__ with your Auth0 ClientId and Domain
+var lock = new Auth0Lock('__CLIENT_ID__', '__AUTH0_DOMAIN__', {
+  container: 'lock',
+  auth: {
+    redirect: true,
+    redirectUrl: 'http://localhost:8080',
+    responseType: 'token'
+  }
+});
+
+lock.show();
+
+lock.on('authenticated', function(authResult) {
+  displayAuthResults(authResult.accessToken, authResult.idToken);
+});
