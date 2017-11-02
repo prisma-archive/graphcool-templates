6f14e1d21ff5652fd5e9a456a1ba9a849c862e94
fabien0102 <fabien0102@planee.fr>
2017-11-02 21:21:34 +0100

    Connect to graphcool

--- auth/auth0/src/auth0Authentication.ts
+++ auth/auth0/src/auth0Authentication.ts
@@ -4,33 +4,43 @@ import { decode } from 'jsonwebtoken'
 import * as jwt from 'express-jwt'
 import * as jwksRsa from 'jwks-rsa'
 
+interface User {
+  id: string
+}
+
 interface EventData {
   idToken: string
 }
 
 export default async (event: FunctionEvent<EventData>) => {
-
   if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_AUDIENCE) {
     console.log('Please provide a valid auth0 domain and audience!')
-    return { error: "Auth0 Authentification not configured correctly." }
+    return { error: 'Auth0 Authentification not configured correctly.' }
   }
 
   try {
-    // const graphcool = fromEvent(event)
-    // const api = graphcool.api('simple/v1')
+    const graphcool = fromEvent(event)
+    const api = graphcool.api('simple/v1')
     const { idToken } = event.data
 
     // check auth0 token
     await checkJwt(idToken)
 
-    console.log(decode(idToken));
+    // get graphcool user with auth0UserId
+    const { sub: auth0UserId } = decode(idToken);
+    const user: User = await getGraphcoolUser(api, auth0UserId)
 
+    // check if graphcool user exists, and create new one if not
+    const userId = user ? user.id : await createGraphcoolUser(api, auth0UserId)
+
+    // generate node token for User node
+    const token = await graphcool.generateNodeToken(userId!, 'User')
+    return { data: { id: userId, token } }
   } catch (e) {
     console.log(e)
     return { error: 'An unexpected error occured during authentication.' }
   }
 }
-
 
 async function checkJwt(idToken: string) {
   // Inject idToken in a header to permit express middleware usage.
@@ -53,4 +63,34 @@ async function checkJwt(idToken: string) {
       algorithms: ['RS256']
     })(req, null, next)
   })
+}
+
+async function getGraphcoolUser(api: GraphQLClient, auth0UserId: string): Promise<User> {
+  return api
+    .request<{ User: User }>(
+    `
+        query getUser($auth0UserId: String!) {
+          User(auth0UserId: $auth0UserId) {
+            id
+          }
+        }
+      `,
+    { auth0UserId }
+    ).then(res => res.User)
+}
+
+async function createGraphcoolUser(api: GraphQLClient, auth0UserId: string): Promise<string> {
+  return api
+    .request<{ createUser: User }>(
+    `
+      mutation createUser($auth0UserId: String!) {
+        createUser(
+          auth0UserId: $auth0UserId
+        ){
+          id
+        }
+      }
+    `,
+    { auth0UserId }
+    ).then(res => res.createUser.id)
 }
