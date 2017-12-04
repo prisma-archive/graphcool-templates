94e94a553e04bb9dd83a143839bee88105fc72f3
Donal Farrell <donskifarrell@googlemail.com>
2017-12-04 15:05:03 +0000

    Fix: Email/Password auth salt not being used in hash
    The `bcrypt.hash` function was not using the generated salt value, so everybody was using `SALT_ROUNDS` only.

--- auth/email-password/src/signup.ts
+++ auth/email-password/src/signup.ts
@@ -36,7 +36,7 @@ export default async (event: FunctionEvent<EventData>) => {
 
     // create password hash
     const salt = bcrypt.genSaltSync(SALT_ROUNDS)
-    const hash = await bcrypt.hash(password, SALT_ROUNDS)
+    const hash = await bcrypt.hash(password, salt)
 
     // create new user
     const userId = await createGraphcoolUser(api, email, hash)
